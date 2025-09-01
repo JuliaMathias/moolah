5fc925f7a7923f299c65abc09592d2712634b7d8
JuliaMathias <juliamss@gmail.com>
2025-09-01 16:01:32 -0300

    fix credo issues

--- lib/moolah_web.ex
+++ lib/moolah_web.ex
@@ -88,8 +88,8 @@ defmodule MoolahWeb do
       import MoolahWeb.CoreComponents
 
       # Common modules used in templates
-      alias Phoenix.LiveView.JS
       alias MoolahWeb.Layouts
+      alias Phoenix.LiveView.JS
 
       # Routes generation with the ~p sigil
       unquote(verified_routes())
